6931a58c43532880027837d14a8dc682c6a90240
Sebastian Rehm <rehmsebastian@gmail.com>
2018-11-19 22:17:23 +0100

    Add initial version

--- src/App.js
+++ src/App.js
@@ -1,28 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useEffect } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const apiKey = process.env.REACT_APP_GIPHY_API_KEY;
+
+const SearchBar = ({ query, dispatch }) => {
+  return (
+    <input
+      name="search"
+      value={query}
+      placeholder="your search term"
+      style={{ marginBottom: "20px" }}
+      onChange={event =>
+        dispatch({ type: "CHANGE_QUERY", query: event.target.value })
+      }
+    />
+  );
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_QUERY":
+      return { ...state, ...{ query: action.query } };
+    case "START_LOADING":
+      return { ...state, ...{ loading: true } };
+    case "GIFS_LOADED":
+      return { ...state, ...{ gifUrls: action.gifUrls, loading: false } };
+    default:
+      return state;
   }
-}
+};
+
+const fetchGifs = async (query, dispatch) => {
+  dispatch({ type: "START_LOADING" });
+  const result = await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}`
+  );
+  const giphyJson = await result.json();
+
+  const gifUrls = giphyJson.data.map(gif => gif.images.fixed_width.url);
+
+  dispatch({ type: "GIFS_LOADED", gifUrls });
+};
+
+const Giphs = ({ gifUrls }) => {
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {gifUrls.map((url, index) => (
+        <img
+          src={url}
+          key={index}
+          style={{ minWidth: "200px", flex: 1, display: "block" }}
+          alt="a gif"
+        />
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    query: "",
+    gifUrls: [],
+    loading: false
+  });
+
+  useEffect(
+    () => {
+      if (state.query.length > 2) {
+        fetchGifs(state.query, dispatch);
+      }
+    },
+    [state.query]
+  );
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <SearchBar query={state.query} dispatch={dispatch} />
+
+      {state.loading ? "Loading..." : <Giphs gifUrls={state.gifUrls} />}
+    </div>
+  );
+};
 
 export default App;
